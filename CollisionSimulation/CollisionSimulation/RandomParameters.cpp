f4c57f45dfa7037264947ead708e3b0cdae784fb
Xavier <xavieroh05@gmail.com>
2018-10-01 21:22:51 +0800

    Added Cr_ref for hydrogen in random parameters

--- CollisionSimulation/CollisionSimulation/RandomParameters.cpp
+++ CollisionSimulation/CollisionSimulation/RandomParameters.cpp
@@ -13,7 +13,20 @@ float RandomParameters::get_DRef() {
 }
 
 float RandomParameters::get_CrRef() {
-	return 0.0f;
+	//Boltzmann_constant
+	float k = 1.380658 * pow(10, -23); //JK^-1
+	float coefficient_of_viscosity = 0.845 * pow(10, -5); //Hydrogen : 0.845 * 10^-5
+	float mass = 3.34 * pow(10, -27); //Hydrogen : 3.34 * 10^-27 kg
+	float viscosity_index = 0.67; //Hydrogen : 0.67
+	float v = viscosity_index - 0.5;
+	//Temperature in kelvins
+	float T = 298.0;
+	//Total collision cross section
+	float sigmaT = 3.1415 * 1.2 * pow(10, -10) * 1.2 * pow(10, -10); //Diameter of hydrogen : 1.2 * 10^-10
+	float inner = (15/8 * sqrt(3.1415*mass*k) * pow(4*k/mass, v) * pow(T, 0.5+v)) / 
+					(viscosity_index * sigmaT * tgamma(4-v));
+	float Cr_ref = pow(sqrt(inner), 1/v);
+	return Cr_ref;
 }
 
 float RandomParameters::get_Cr() {
