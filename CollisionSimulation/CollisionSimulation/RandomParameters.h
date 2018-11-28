fdcd51671208cd2705bb326ce5328e633c7a4feb
Admin <h1410115@nushigh.edu.sg>
2018-11-28 20:26:33 +0800

    Added getter methods for randomparams Added randomparam values to CollDy

--- CollisionSimulation/CollisionSimulation/RandomParameters.h
+++ CollisionSimulation/CollisionSimulation/RandomParameters.h
@@ -3,9 +3,9 @@
 
 class RandomParameters {
 public:
+	RandomParameters();
 	double viscosity_index = 0.67;
 	double coefficient_of_viscosity = 0.845 * pow(10, -5); //Hydrogen : 0.845 * 10^-5
-	RandomParameters();
 	double get_DRef();
 	double get_CrRef();
 	void get_3D_Cr(double* cr_vector);
@@ -19,9 +19,14 @@ public:
 	void setDiameter(double diameter);
 	void setTemperature(double T);
 
+	double getCoefficientOfViscosity();
+	double getMass();
+	double getViscosityIndex();
+	double getDiameter();
+	double getTemperature();
+
 private:
-	//Boltzmann_constant
-	double k = 1.380658 * pow(10, -23); 
+	double k = 1.380658 * pow(10, -23); //Boltzmann_constant
 	
 	double mass = 3.34 * pow(10, -27); //Hydrogen : 3.34 * 10^-27 kg
 	
@@ -30,8 +35,8 @@ private:
 	double diameter = 2.92 * pow(10, -10); //Hydrogen 2.92 * 10^-10
 	
 	double radius = diameter / 2;
-	//Temperature in kelvins
-	double T = 273.0;
+	
+	double T = 273.0; //Temperature in kelvins
 	
 	double density = 1.292; //Density of air kg/m^3
 };
